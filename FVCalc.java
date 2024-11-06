34b0c21f96be3f9b02947e9903cecce9c9ec5312
Noa Salton <noasalton@Noas-MacBook-Pro.local>
2024-11-06 18:43:34 +0200

    Changes in Calc..

--- FVCalc.java
+++ FVCalc.java
@@ -2,10 +2,10 @@
 public class FVCalc {
 	public static void main(String[] args){
 		int currentVal = Integer.parseInt(args[0]);
-		double rate = Double.parseDouble(args[1]);
+		double rate = Double.parseDouble(args[1])/100.0;
 		int years = Integer.parseInt(args[2]);
 		System.out.println("After " + years + " years, $" + currentVal + " saved at " +
 		rate + "% will yield $" + 
-		(int)(currentVal*Math.pow((rate + 1.0), (double)years)/100));
+		(int)(currentVal*Math.pow((rate + 1.0), (double)years)));
 	}
 }
