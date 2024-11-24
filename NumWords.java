d9ac13c83e5abae25849263e51ff1d246b4ade6a
YamlRUNI <155106022+YamlRUNI@users.noreply.github.com>
2024-11-24 17:32:51 +0200

    Update NumWords.java

--- NumWords.java
+++ NumWords.java
@@ -1,4 +1,9 @@
 // Prints a given number using a hundreds, tens, and units notation.
+
+// Hey Noa, I dont see ay implementation here.
+// did something go wrong during solving?
+
+
 public class NumWords {
 	public static void main(String args[]) {
 	    // Replace this comment with your code
